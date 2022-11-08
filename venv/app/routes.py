6979bc6802ab501bcf76b93f688973985b642f1f
Iliyana Pirinska <40432179@live.napier.ac.uk>
2022-11-08 00:12:08 +0000

    adding html

--- venv/app/routes.py
+++ venv/app/routes.py
@@ -3,5 +3,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Vesko'}
+    return '''
+    <html> 
+    <head>
+        <title>Home Page - Welcome</title>
+    </head>
+    <body>
+        <h1>Welcome, ''' + user['username'] + '''!</h1>
+    </body>
+
+    </html> '''
 
