ddaf7ceb9866c641d78af1b8aa885344ebf72aae
Iliyana Pirinska <40432179@live.napier.ac.uk>
2022-11-18 01:07:35 +0000

    Creating database, migration - User, Todo list

--- venv/app/routes.py
+++ venv/app/routes.py
@@ -1,13 +1,18 @@
-from flask import Flask, request, render_template
+from flask import request, render_template, redirect, flash, url_for
 from app import app
 from app.form import LoginForm
 
+
 @app.route('/')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form=LoginForm()
-    return render_template('login.html', title='Login', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
     
 @app.route('/register')
 def register():
