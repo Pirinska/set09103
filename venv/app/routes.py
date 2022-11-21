737874ff0f97ad3e3556852e027855ea75b3c270
Iliyana Pirinska <40432179@live.napier.ac.uk>
2022-11-21 14:48:36 +0000

    Before working on database

--- venv/app/routes.py
+++ venv/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, 
+from flask import Flask, request, render_template
 from app import app
 
 
@@ -6,28 +6,33 @@ from app import app
 @app.route('/')
 
 
-@app.route('/login')
+@app.route('/register', methods=['GET', 'post'])
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route('/login',  methods=['GET', 'post'])
 def login():
-    user = {'username': 'Vili'}
-    return render_template('login.html', title='Home', user=user)
+    return render_template('login.html', title='Login')
+
 
 @app.route('/index')
 def index():
-    user = {'username': 'Vili'}
-    return render_template('index.html', title='Home', user=user)
+    
+    return render_template('index.html', title='Home')
 
 @app.route('/calculate')
 def calculate():
-    user = {'username': 'Vili'}
-    return render_template('calculate.html', title='Calculate', user=user)
+    
+    return render_template('calculate.html', title='Calculate')
 
 @app.route('/plan')
 def plan():
-    user = {'username': 'Vili'}
-    return render_template('plan.html', title='Plan', user=user)
+    
+    return render_template('plan.html', title='Plan')
 
 @app.route('/userProfile')
 def userProfile():
-    user = {'username': 'Vili'}
-    return render_template('userProfile.html', title='User Profile', user=user)
+    
+    return render_template('userProfile.html', title='User Profile')
+
 
