6b0145b960550933644f63a8c6f628bd298754ca
Iliyana Pirinska <78374576+Pirinska@users.noreply.github.com>
2022-11-28 16:36:01 +0000

    set09103 change

---
+++ set09103.py
@@ -0,0 +1,16 @@
+# from .routes import views, db
+# from app.models import User, Todo, MeasureLogs
+
+# # The shell processor allows to use Python commands and retrieve data from the database
+
+
+# @views.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Todo': Todo, 'MeasureLogs': MeasureLogs}
+
+from venv import create_app
+
+app=create_app
+if __name__ == '__main__':
+    app.run(debug=True)
+
